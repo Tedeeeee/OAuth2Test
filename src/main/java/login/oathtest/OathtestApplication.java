a4d0ce9ced1c0db93437343bed38bf1b8babf5fb
Tedeeeee <gksrmf359@gmail.com>
2023-05-11 15:01:45 +0900

    new start

---
+++ src/main/java/login/oathtest/OathtestApplication.java
@@ -0,0 +1,13 @@
+package login.oathtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OathtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OathtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/login/oathtest/OathtestApplicationTests.java
@@ -0,0 +1,13 @@
+package login.oathtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OathtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
